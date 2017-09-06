01e32a35305fdad8a5568d5da0ffcde7f32a625f
chajr <chajr@bluetree.pl>
2017-09-06 22:19:17 +0200

    Small refactor

--- src/Storage/File.php
+++ src/Storage/File.php
@@ -28,18 +28,13 @@ class File implements StorageInterface
         //convert expire to timestamp
 
         $cacheFile = $this->params['cache_path'] . DIRECTORY_SEPARATOR . $name . '_' . $expire . '.cache';
+        $dir = $this->params['cache_path'];
 
-        if (!file_exists($this->params['cache_path'])) {
-            $bool = mkdir($this->params['cache_path']);
-
-            if (!$bool) {
-                throw new CacheException('Unable to create cache directory: ' . $this->params['cache_path']);
-            }
+        if (!mkdir($dir) && !is_dir($dir)) {
+            throw new CacheException('Unable to create cache directory: ' . $this->params['cache_path']);
         }
 
-        $bool = file_put_contents($cacheFile, serialize($data));
-
-        if (!$bool) {
+        if (!file_put_contents($cacheFile, serialize($data))) {
             throw new CacheException('Unable to save log file: ' . $cacheFile);
         }
 
@@ -56,7 +51,7 @@ class File implements StorageInterface
         
     }
 
-    protected function exists($name)
+    public function exists($name)
     {
         //find with regexp
         return ;
