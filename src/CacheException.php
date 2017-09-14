0b6028a030f93b36f8268ad485a02328623d90e7
chajr <chajr@bluetree.pl>
2017-09-14 18:29:16 +0200

    Added CacheException

---
+++ src/CacheException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BlueCache;
+
+use Exception;
+use Psr\Cache\CacheException as CacheExceptionInterface;
+
+/**
+ * Exception interface for all exceptions thrown by an Implementing Library.
+ */
+class CacheException extends Exception implements CacheExceptionInterface
+{
+}
