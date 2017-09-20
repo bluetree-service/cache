5e9cdb6b1a60e0fad7d35f499e51a6bce1a2dc93
chajr <chajr@bluetree.pl>
2017-09-20 19:43:56 +0200

    Added CacheItem tests

---
+++ tests/CacheItemTest.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace BlueCache\Test;
+
+use BlueCache\CacheItem;
+use PHPUnit\Framework\TestCase;
+
+class CacheItemTest extends TestCase
+{
+    public function testCreateCacheItem()
+    {
+        $item = new CacheItem('test');
+
+        $this->assertEquals($item->getKey(), 'test');
+        $this->assertFalse($item->isHit());
+
+        $item->set('test data');
+
+        $this->assertEquals($item->get(), 'test data');
+    }
+
+    /**
+     * @expectedException \BlueCache\CacheException
+     * @expectedExceptionMessage Invalid key. Should us only chars, numbers and _. Use: test+1234
+     */
+    public function testCreateCacheItemWithException()
+    {
+        new CacheItem('test+1234');
+    }
+
+    public function testCacheItemDefaultExpire()
+    {
+        $item = (new CacheItem('test', ['expire' => 1]))->set('test data');
+
+        $this->assertTrue($item->isHit());
+
+        sleep(2);
+
+        $this->assertTrue($item->isHit());
+
+        $item->expiresAfter();
+
+        $this->assertTrue($item->isHit());
+
+        sleep(2);
+
+        $this->assertFalse($item->isHit());
+        $this->assertNull($item->get());
+    }
+
+    public function testCacheItemExpireAt()
+    {
+        $item = (new CacheItem('test'))->set('test data');
+
+        $this->assertTrue($item->isHit());
+        $item->expiresAt(2);
+
+        sleep(2);
+
+        $this->assertFalse($item->isHit());
+
+        $item->set('test data');
+        $this->assertTrue($item->isHit());
+
+        $item->expiresAt(new \DateTime('+2 seconds'));
+
+        sleep(3);
+
+        $this->assertFalse($item->isHit());
+    }
+
+    public function testCacheItemExpireAfter()
+    {
+        $item = (new CacheItem('test'))->set('test data');
+
+        $this->assertTrue($item->isHit());
+        $item->expiresAfter(5);
+
+        sleep(2);
+
+        $this->assertFalse($item->isHit());
+
+        $item->set('test data');
+        $this->assertTrue($item->isHit());
+
+        $item->expiresAfter(new \DateInterval('P0Y0DT0H0M2S'));
+
+        sleep(3);
+
+        $this->assertFalse($item->isHit());
+    }
+
+    /**
+     * @expectedException \BlueCache\CacheException
+     * @expectedExceptionMessage Invalid expire type.
+     */
+    public function testCacheItemExpireWithError()
+    {
+        $item = (new CacheItem('test'))->set('test data');
+
+        $this->assertTrue($item->isHit());
+        $item->expiresAfter('foo');
+    }
+}
