c3d59edf6ec4f653fd3e537dc37d1d242d60e486
chajr <chajr@bluetree.pl>
2017-10-04 20:06:51 +0200

    Added some new tests for Cache

--- tests/CacheTest.php
+++ tests/CacheTest.php
@@ -3,10 +3,23 @@
 namespace BlueCache\Test;
 
 use BlueCache\Cache;
+use BlueCache\CacheItem;
 use PHPUnit\Framework\TestCase;
 
 class CacheTest extends TestCase
 {
+    /**
+     * store generated cache file path
+     *
+     * @var string
+     */
+    protected $cachePath;
+
+    /**
+     * @var string
+     */
+    protected $fullTestFilePath;
+
     public function testCreateCache()
     {
         $items = (new Cache)->getItems();
@@ -37,14 +50,107 @@ class CacheTest extends TestCase
         $this->assertEmpty($items);
     }
 
+    /**
+     * @expectedException \BlueCache\CacheException
+     * @expectedExceptionMessage Incorrect storage type: BlueCache\Cache
+     */
     public function testCreateCacheWithIncorrectStorage()
     {
-        $items = (new Cache([
-            [
-                'storage_class' => '\noExists',
-            ]
-        ]))->getItems();
+        new Cache(['storage_class' => Cache::class]);
+    }
 
-        $this->assertEmpty($items);
+    /**
+     * @expectedException \BlueCache\CacheException
+     */
+    public function testCreateCacheWithIncorrectStorageType()
+    {
+        new Cache(['storage_class' => 123123]);
+    }
+
+    public function testAddItemToCache()
+    {
+        $cache = new Cache([
+            'storage_directory' => $this->cachePath
+        ]);
+        $item = (new CacheItem('test'))->set('test data');
+
+        $this->assertFalse($cache->hasItem('test'));
+
+        $cache->save($item);
+
+        $this->assertTrue($cache->hasItem('test'));
+    }
+
+    public function testGetItemFromCache()
+    {
+        $cache = new Cache([
+            'storage_directory' => $this->cachePath
+        ]);
+        $item = (new CacheItem('test'))->set('test data');
+
+        $this->assertFalse($cache->hasItem('test'));
+
+        $cache->save($item);
+
+        $this->assertTrue($cache->hasItem('test'));
+
+        $item = $cache->getItem('test');
+
+        $this->assertInstanceOf(CacheItem::class, $item);
+        $this->assertEquals('test data', $item->get());
+
+        $items = $cache->getItems(['test']);
+
+        $this->assertInstanceOf(CacheItem::class, $items['test']);
+        $this->assertEquals('test data', $items['test']->get());
+    }
+
+    public function testClearItems()
+    {
+        $cache = new Cache([
+            'storage_directory' => $this->cachePath
+        ]);
+        $item = (new CacheItem('test'))->set('test data');
+
+        $this->assertFalse($cache->hasItem('test'));
+
+        $cache->save($item);
+
+        $this->assertTrue($cache->hasItem('test'));
+
+        $cache->clear();
+
+        $this->assertFalse($cache->hasItem('test'));
+    }
+
+    public function testDeleteItems()
+    {
+        
+    }
+
+    public function testSaveDeferred()
+    {
+        
+    }
+
+    /**
+     * actions launched before test starts
+     */
+    protected function setUp()
+    {
+        $this->cachePath = dirname(__DIR__) . '/tests/var/cache';
+        $this->fullTestFilePath = $this->cachePath . '/test.cache';
+
+        $this->tearDown();
+    }
+
+    /**
+     * actions launched after test was finished
+     */
+    protected function tearDown()
+    {
+        if (file_exists($this->fullTestFilePath)) {
+            unlink($this->fullTestFilePath);
+        }
     }
 }
