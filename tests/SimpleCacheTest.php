e11604b3353b1f9fdbf550d57b608174f8013c3a
chajr <chajr@bluetree.pl>
2017-10-21 20:26:34 +0200

    Added some basic tests

--- tests/SimpleCacheTest.php
+++ tests/SimpleCacheTest.php
@@ -7,5 +7,81 @@ use PHPUnit\Framework\TestCase;
 
 class SimpleCacheTest extends TestCase
 {
-    
+    /**
+     * store generated cache file path
+     *
+     * @var string
+     */
+    protected $cachePath;
+
+    /**
+     * @var string
+     */
+    protected $fullTestFilePath;
+
+    public function testSimpleCacheObject()
+    {
+        $this->assertInstanceOf(SimpleCache::class, new SimpleCache);
+    }
+
+    public function testCreateSimpleCacheWithConfig()
+    {
+        $cache = new SimpleCache(
+            [
+                'storage_class' => \BlueCache\Storage\File::class,
+                'storage_directory' => './var/cache',
+            ]
+        );
+
+        $this->assertInstanceOf(SimpleCache::class, $cache);
+    }
+
+    public function testCreateSimpleCacheWithDefinedStorage()
+    {
+        $storage = new \BlueCache\Storage\File;
+
+        $cache = new SimpleCache([
+            'storage_class' => $storage,
+        ]);
+
+        $this->assertInstanceOf(SimpleCache::class, $cache);
+    }
+
+    /**
+     * @expectedException \BlueCache\CacheException
+     * @expectedExceptionMessage Incorrect storage type: BlueCache\SimpleCache
+     */
+    public function testCreateSimpleCacheWithIncorrectStorage()
+    {
+        new SimpleCache(['storage_class' => SimpleCache::class]);
+    }
+
+    /**
+     * @expectedException \BlueCache\CacheException
+     */
+    public function testCreateSimpleCacheWithIncorrectStorageType()
+    {
+        new SimpleCache(['storage_class' => 123123]);
+    }
+
+    /**
+     * actions launched before test starts
+     */
+    protected function setUp()
+    {
+        $this->cachePath = dirname(__DIR__) . '/tests/var/cache';
+        $this->fullTestFilePath = $this->cachePath . '/test.cache';
+
+        $this->tearDown();
+    }
+
+    /**
+     * actions launched after test was finished
+     */
+    protected function tearDown()
+    {
+        if (file_exists($this->fullTestFilePath)) {
+            unlink($this->fullTestFilePath);
+        }
+    }
 }
