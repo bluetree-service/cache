0e4d8453b3569890fa71de5ab6100c03f7ef47a2
chajr <chajr@bluetree.pl>
2017-10-24 19:57:24 +0200

    Added some simple cache logic

--- src/SimpleCache.php
+++ src/SimpleCache.php
@@ -8,14 +8,42 @@ class SimpleCache implements CacheInterface
 {
     use Common;
 
+    /**
+     * @param string $key
+     * @param mixed $default
+     * @return array|null|\Psr\Cache\CacheItemInterface
+     */
     public function get($key, $default = null)
     {
+        $cacheItem = $this->storage->restore($key);
 
+        if (is_null($cacheItem)) {
+            return $default;
+        }
+
+        return $cacheItem->get();
     }
 
+    /**
+     * @param string $key
+     * @param mixed $value
+     * @param null $ttl
+     * @return $this
+     * @throws \BlueCache\CacheException
+     */
     public function set($key, $value, $ttl = null)
     {
+        $config = [];
 
+        if (!is_null($ttl)) {
+            $config['expire'] = $ttl;
+        }
+
+        $item = (new CacheItem($key, $config))->set($value);
+
+        $this->storage->store($item);
+
+        return $this;
     }
 
     public function delete($key)
@@ -43,8 +71,12 @@ class SimpleCache implements CacheInterface
 
     }
 
+    /**
+     * @param string $key
+     * @return bool
+     */
     public function has($key)
     {
-
+        return $this->storage->exists($key);
     }
 }
